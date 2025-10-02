36eb23956dc026b854ffec63fa5a8c3f70b35926
ivankp <ivan.pogrebnyak@gmail.com>
2025-10-01 21:46:10 -0400

    compilation-timing: compare with #include <iostream>

--- tests/compilation-timing/test.sh
+++ tests/compilation-timing/test.sh
@@ -9,14 +9,16 @@ set -e
 TIMEFORMAT='%S %U'
 
 time_compilation() {
-    time \
-        g++ -std=c++17 -Wall -O3 -I../../include "$@" -o /dev/null
+    time g++ -std=c++17 -Wall -O3 -I../../include "$@" -o /dev/null
 }
 
 N="$1"
 for (( i = 0; i < N; ++i )); do
-    time_compilation with-visit.cpp 2>&1
     time_compilation without-visit.cpp 2>&1
+    time_compilation with-visit.cpp 2>&1
+    time_compilation -xc++ \
+        <(sed '1s/^/#include <iostream>\n/' without-visit.cpp) \
+        2>&1
 done | python -c '''
 import sys, math
 ts = [ sum(float(t) for t in line.split()) for line in sys.stdin ]
@@ -25,14 +27,19 @@ def mean_stdev(xs):
     m = sum(xs)/n
     s = math.sqrt(sum( (x - m)**2 for x in xs ) / (n - 1))
     return (m, s)
-t = [ mean_stdev(ts[i::2]) for i in (0,1) ]
-d = min(math.ceil(-math.log10(min(t[1] for t in t))) + 1, 6)
-dt = (t[0][0] - t[1][0], math.sqrt(t[0][1]**2 + t[1][1]**2))
+n = 3
+t = [ mean_stdev(ts[i::n]) for i in range(n) ]
+dt = [
+    (t[i][0] - t[0][0], math.sqrt(t[i][1]**2 + t[0][1]**2))
+    for i in range(1,n)
+]
 def p(m,s):
-    return f"{m:.{d}f} ± {s:.{d}f}"
+    return f"{m:.6f} ± {s:.6f}"
 print(f"""\
-With    visit: { p(*t[0]) }
-Without visit: { p(*t[1]) }
-Difference   : { p(*dt) }\
+Without visit   : { p(*t[0]) }
+With    visit   : { p(*t[1]) }
+Difference      : { p(*dt[0]) }
+Include iostream: { p(*t[2]) }
+Difference      : { p(*dt[1]) }\
 """)
 '''
